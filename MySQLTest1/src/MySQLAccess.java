d1ba75f60ca45e4da80343061a8c92fc1254b1a0
hvvivek1977 <hv.vivek@gmail.com>
2019-01-31 16:39:44 +0530

    Adding nee conflicting comment on Windows
    Adding nee conflicting comment on Windows

--- MySQLTest1/src/MySQLAccess.java
+++ MySQLTest1/src/MySQLAccess.java
@@ -1,7 +1,7 @@
 
 //STEP 1. Import required packages
 import java.sql.*;
-
+//Adding conflicting comment
 public class MySQLAccess {
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
